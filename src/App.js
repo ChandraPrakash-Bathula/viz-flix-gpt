0cd5d75076b5d33580a1a567878c4811d12dfa86
Chandra Prakash Bathula <chandraprakashbathula@Chandras-MacBook-Pro.local>
2024-03-30 16:25:23 -0500

    login and sign up pages created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import './App.css';
-
+import Body from "./Components/Body"
 function App() {
   return (
-    <div className="text-center font-bold text-2xl text-green-900 p-4 m-4">
-   <h1>Hello People!</h1>
-    </div>
+   <Body />
   );
 }
 
