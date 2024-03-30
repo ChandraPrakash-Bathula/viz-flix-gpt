a845193a44f562baa9ae6320121ff6be4187fb99
Chandra Prakash Bathula <chandu.bathula16@gmail.com>
2024-03-30 14:20:04 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center font-bold text-2xl text-green-900 p-4 m-4">
+   <h1>Hello People!</h1>
+    </div>
+  );
+}
+
+export default App;
