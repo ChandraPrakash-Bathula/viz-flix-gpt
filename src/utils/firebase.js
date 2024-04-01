25a7e7b79132b14c88e18a2aaf796ee50e1b9539
Chandra Prakash Bathula <chandu.bathula16@gmail.com>
2024-04-01 17:07:59 -0500

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,11 +1,14 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyBT7YoZoyzwOncnBI456r0twiVltHgCfU8",
   authDomain: "vizflixgpt.firebaseapp.com",
